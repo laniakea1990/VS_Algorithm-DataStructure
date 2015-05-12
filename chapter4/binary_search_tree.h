dea835d92c5757e252f145d96b54754aacaf17d6
raiseking <jinranran1990910@163.com>
2015-05-12 21:41:49 +0800

    0512

--- chapter4/binary_search_tree.h
+++ chapter4/binary_search_tree.h
@@ -45,7 +45,7 @@ Position Find(int X, SearchTree T)
 		return T;
 }
 
-Position FindMin(SearchTree T)
+Position FindMin(SearchTree T)		//递归实现
 {
 	if (T == NULL)
 		return NULL;
@@ -57,7 +57,7 @@ Position FindMin(SearchTree T)
 		return FindMin(T->left);
 }
 
-Position FindMax(SearchTree T)
+Position FindMax(SearchTree T)		//非递归实现
 {
 	if (T != NULL)
 		while (T -> right != NULL)
@@ -85,6 +85,37 @@ SearchTree Insert(int X, SearchTree T)
 		T->left = Insert(X, T->left);
 	else if (X > T->Element)
 		T->right = Insert(X, T->right);
+	/*Else X is in the tree already; we'll do nothing*/
 
 	return T;	//Do not forget this line!!
+}
+
+SearchTree Delete(int X, SearchTree T)
+{
+	Position TmpCell;
+
+	if (T == NULL)
+		ERROR("Element not found");
+	else if (X < T->Element)	/* Go left */
+		T->left = Delete(X, T->left);
+	else if (X > T->Element)	// Go right
+		T->right = Delete(X, T->right);
+	else if (T->left&&T->right)	//Found! Two children
+	{
+		// Replace with smallest in right subtree
+		TmpCell = FindMin(T->right);
+		T->Element = TmpCell->Element;
+		T->right = Delete(T->Element, T->right);
+	}
+	else     //Found! One or zero children
+	{
+		TmpCell = T;
+		if (T->left = NULL)	// Also handles 0 children
+			T = T->right;
+		else if (T->right = NULL)
+			T = T->left;
+		free(TmpCell);
+	}
+
+	return T;
 }
