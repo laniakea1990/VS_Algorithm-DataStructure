983ea9a3dd0c930e66cc37ec35a88c8617898e4a
raiseking <jinranran1990910@163.com>
2015-05-20 09:32:56 +0800

    0519

---
+++ chapter7---Sort/InsertionSort.cpp
@@ -0,0 +1,15 @@
+#define ElementType int
+
+void InsertionSort(ElementType A[], int N)
+{
+	int j, P;
+
+	ElementType Tmp;
+	for (P = 1; P < N; P++)
+	{
+		Tmp = A[P];
+		for (j = P; j > 0 && A[j - 1] > Tmp; j--)
+			A[j] = A[j - 1];	// 没有明显使用交换
+		A[j] = Tmp;
+	}
+}
