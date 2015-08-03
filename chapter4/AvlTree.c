148761889d60621464b7541661297b79c0de7a33
raiseking <jinranran1990910@163.com>
2015-08-03 16:26:01 +0800

    0803

--- chapter4/AvlTree.c
+++ chapter4/AvlTree.c
@@ -40,19 +40,19 @@ AvlTree Insert(ElementType X, AvlTree T)
 	{
 		T->Left = Insert(X, T->Left);
 		if (Height(T->Left) - Height(T->Right) == 2)
-		if (X < T->Left->Element)
-			T = SingleRotateWithLeft(T);
-		else
-			T = DoubleRotateWithLeft(T);
+			if (X < T->Left->Element)
+				T = SingleRotateWithLeft(T);
+		    else
+				T = DoubleRotateWithLeft(T);
 	}
 	else if (X > T->Element)
 	{
 		T->Right = Insert(X, T->Right);
 		if (Height(T->Right) - Height(T->Left) == 2)
-		if (X>T->Right->Element)
-			T = SingleRotateWithRight(T);
-		else
-			T = DoubleRotateWithRight(T);
+			if (X>T->Right->Element)
+				T = SingleRotateWithRight(T);
+			else
+				T = DoubleRotateWithRight(T);
 	}
 	/* Else X is in the tree already; we'll do nothing */
 
--- chapter7---Sort/InsertionSort.cpp
+++ chapter7---Sort/InsertionSort.cpp
@@ -1,15 +1,15 @@
-//#define ElementType int
-//
-//void InsertionSort(ElementType A[], int N)
-//{
-//	int j, P;
-//
-//	ElementType Tmp;
-//	for (P = 1; P < N; P++)
-//	{
-//		Tmp = A[P];
-//		for (j = P; j > 0 && A[j - 1] > Tmp; j--)
-//			A[j] = A[j - 1];	// 没有明显使用交换
-//		A[j] = Tmp;
-//	}
-//}
+#define elementtype int
+
+void insertionsort(elementtype a[], int n)
+{
+	int j, p;
+
+	elementtype tmp;
+	for (p = 1; p < n; p++)
+	{
+		tmp = a[p];
+		for (j = p; j > 0 && a[j - 1] > tmp; j--)
+			a[j] = a[j - 1];	// 没有明显使用交换
+		a[j] = tmp;
+	}
+}
